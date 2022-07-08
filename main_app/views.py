50733a186f045a2bb0a35dca8c1592835d26b71e
“julian <badjab326@gmail.com>
2022-07-08 10:54:08 -0700

    prep for heroku deploy

---
+++ main_app/views.py
@@ -0,0 +1,148 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Cat, Toy, Photo
+from .forms import FeedingForm
+import uuid
+import boto3
+
+S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
+BUCKET = 'catcollector-avatar-326'
+
+
+# Create your views here.
+
+# def home(request):
+#     '''
+#     this is where we return a response
+#     in most cases we  would render a template
+#     and we'll need some data for that template
+#     '''
+#     return HttpResponse('<h1> Hello World </h1>')
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+@login_required
+def cats_index(request):
+  cats = request.user.cat_set.all()
+  return render(request, 'cats/index.html', { 'cats': cats })
+
+@login_required
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  feeding_form = FeedingForm()
+  toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
+  return render(request, 'cats/detail.html', {
+    # include the cat and feeding_form in the context
+    'cat': cat, 'feeding_form': feeding_form,
+    'toys': toys_cat_doesnt_have
+  })
+
+@login_required
+def add_feeding(request, cat_id):
+  # create the ModelForm using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
+
+@login_required
+def assoc_toy(request, cat_id, toy_id):
+  Cat.objects.get(id=cat_id).toys.add(toy_id)
+  return redirect('detail', cat_id=cat_id)
+
+@login_required
+def assoc_toy_delete(request, cat_id, toy_id):
+  Cat.objects.get(id=cat_id).toys.remove(toy_id)
+  return redirect('detail', cat_id=cat_id)
+
+@login_required
+def add_photo(request, cat_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      photo = Photo(url=url, cat_id=cat_id)
+      photo.save()
+    except Exception as error:
+      print("Error uploading photo: ", error)
+      return redirect('detail', cat_id=cat_id)
+  return redirect('detail', cat_id=cat_id)
+
+
+class CatCreate(LoginRequiredMixin, CreateView):
+  model = Cat
+  fields = ['name', 'breed', 'description', 'age']
+  success_url = '/cats/'
+  # This inherited method is called when a
+  # valid cat form is being submitted
+  def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+    form.instance.user = self.request.user  # form.instance is the cat
+    # Let the CreateView do its job as usual
+    return super().form_valid(form)
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
+  model = Cat
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = [ 'breed', 'description', 'age']
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+  model = Cat
+  success_url = '/cats/'
+
+class ToyList(LoginRequiredMixin, ListView):
+  model = Toy
+  template_name = 'toys/index.html'
+
+class ToyDetail(LoginRequiredMixin, DetailView):
+  model = Toy
+  template_name = 'toys/detail.html'
+
+class ToyCreate(LoginRequiredMixin, CreateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyUpdate(LoginRequiredMixin, UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(LoginRequiredMixin, DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid info - please try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
