b8aa415c8249115520cdea2002e9d5907fe1acbd
“julian <badjab326@gmail.com>
2022-07-08 11:36:56 -0700

    dependencies updated

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn catcollector2.wsgi
+web: gunicorn catcollector.wsgi
--- catcollector/settings.py
+++ catcollector/settings.py
@@ -121,5 +121,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import django_heroku
 django_heroku.settings(locals())
-
-WSGI_APPLICATION = 'catcollector2.wsgi.application'
